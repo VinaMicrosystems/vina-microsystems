c7bda6ef3731b79b78a823723b36ba5fd3fe0f92
slax425 <murgornaftali@gmail.com>
2020-07-19 13:25:21 +0300

    Updates

---
+++ precache-manifest.492b3e506c27137357b6c6bca08bd6ee.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb10390bf047a873fb275cea69306c4",
+    "url": "/vina/index.html"
+  },
+  {
+    "revision": "02552a5ed8c336ba8aa3",
+    "url": "/vina/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "66579af7e1a40cb1bbde",
+    "url": "/vina/static/css/main.7baa1ad7.chunk.css"
+  },
+  {
+    "revision": "02552a5ed8c336ba8aa3",
+    "url": "/vina/static/js/2.6c6250de.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/vina/static/js/2.6c6250de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66579af7e1a40cb1bbde",
+    "url": "/vina/static/js/main.710bc268.chunk.js"
+  },
+  {
+    "revision": "70ec05de0d027ac0963a",
+    "url": "/vina/static/js/runtime-main.c2e3d29c.js"
+  },
+  {
+    "revision": "102aa357ba9bf501d47ad5dc48a869e2",
+    "url": "/vina/static/media/Muindejk.102aa357.jpeg"
+  },
+  {
+    "revision": "28e92852b7e2257af4afc11de2480f2e",
+    "url": "/vina/static/media/collins.28e92852.jpg"
+  },
+  {
+    "revision": "687180834e71818255e449ec6ed473c0",
+    "url": "/vina/static/media/hero-image.68718083.png"
+  },
+  {
+    "revision": "9145faa65286c08b306732e283767e34",
+    "url": "/vina/static/media/iot.9145faa6.png"
+  },
+  {
+    "revision": "04f148f77d3f342af6d9e1e11c4609a9",
+    "url": "/vina/static/media/isometrics.04f148f7.png"
+  },
+  {
+    "revision": "6715814ac66a11c7891eb23ac7d3c6e8",
+    "url": "/vina/static/media/murgor.6715814a.jpg"
+  }
+]);
